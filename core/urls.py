fec1cdf320c7a8eb67cf800e459f1ed5e2157383
Daksh Bhatia <daksh001b@gmail.com>
2021-07-25 21:46:54 +0530

    Modified 2 apis

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from core.views import CreateQuiz,SubmitQuiz
 
 urlpatterns = [
-    path('create_quiz/', CreateQuiz.as_view(),name = 'create quiz'),
+    path('create-quiz/', CreateQuiz.as_view(),name = 'create quiz'),
     path('submit-quiz/', SubmitQuiz.as_view(),name = 'submit quiz'),
 ]
