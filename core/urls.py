95ba4eacd748e634f057d977ef0f2d91acf38544
Daksh Bhatia <daksh001b@gmail.com>
2021-07-26 21:37:03 +0530

    Get Quiz api completed

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from core.views import CreateQuiz,SubmitQuiz
+from core.views import CreateQuiz,SubmitQuiz,GetQuiz
 
 urlpatterns = [
     path('create-quiz/', CreateQuiz.as_view(),name = 'create quiz'),
     path('submit-quiz/', SubmitQuiz.as_view(),name = 'submit quiz'),
+    path('get-quiz/', GetQuiz.as_view(),name = 'get quiz'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from core.models import QuizDetails, QuizOptions, Questions
 from django.shortcuts import render
 from . import serializers
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from datetime import datetime
+from datetime import timedelta
+import pytz
 
 #third party imports
 from rest_framework.response import Response
@@ -87,13 +91,33 @@ class CreateQuiz(APIView):
         else:
             return Response(quizDetailsSerializer.errors)      
 
-
-        return Response(quizDetailsSerializer.data)  
+        return Response(quizDetailsSerializer.data)
 
 
 class SubmitQuiz(APIView):
 
     def post(self,request,*args,**kwargs):
+        
+        intz = pytz.timezone('Asia/Kolkata')
+
+        question_id = request.data['answers'][0]['question_id']
+        question = Questions.objects.get(pk = question_id)
+
+        quiz_details_qs = QuizDetails.objects.get(pk = question.quiz_details.pk)
+
+        quiz_datetime = datetime.combine(quiz_details_qs.date, quiz_details_qs.start_time)
+        quiz_datetime = quiz_datetime.replace(tzinfo = intz)
+        quiz_end_datetime = quiz_datetime + quiz_details_qs.duration
+        quiz_end_datetime = quiz_end_datetime.replace(tzinfo = intz)
+        
+        quiz_end_datetime += timedelta(minutes=6)
+        
+        print(quiz_datetime)
+        print(datetime.now(intz))
+        print(quiz_end_datetime)
+        
+        if datetime.now(intz) > quiz_end_datetime:
+            return JsonResponse({"msg":"Quiz has ended. Cannot submit now!"})
 
         for response in request.data['answers']:
 
@@ -108,3 +132,48 @@ class SubmitQuiz(APIView):
 
         return Response(quizAnsweredSerializer.data)
 
+
+class GetQuiz(APIView):
+
+    def get(self,request,*args,**kwargs):
+
+        intz = pytz.timezone('Asia/Kolkata')
+        quiz_details_qs = QuizDetails.objects.prefetch_related('questions').get(id = request.data["quiz_id"])
+        quiz_datetime = datetime.combine(quiz_details_qs.date, quiz_details_qs.start_time)
+        quiz_datetime = quiz_datetime.replace(tzinfo = intz)
+        quiz_end_datetime = quiz_datetime + quiz_details_qs.duration
+        quiz_end_datetime = quiz_end_datetime.replace(tzinfo = intz)
+
+        print(quiz_datetime)
+        print(datetime.now(intz))
+        print(quiz_end_datetime)
+        
+        if quiz_end_datetime > datetime.now(intz):
+            return JsonResponse({"msg" : "Quiz has already ended!"})
+
+        if quiz_datetime < datetime.now(intz):
+            return JsonResponse({"msg" : "Quiz not started yet!"})
+
+        
+        quiz_details = model_to_dict(quiz_details_qs)
+        quiz_details["questions"] = []
+        questions_list = quiz_details_qs.questions
+
+        print(questions_list)
+        questions_qs = Questions.objects.filter(quiz_details = request.data["quiz_id"]).prefetch_related('options')
+
+
+        for question in questions_qs:
+            quiz_details["questions"].append(model_to_dict(question))
+ 
+        for question in quiz_details["questions"]:
+            options_qs = QuizOptions.objects.only("option_name").filter(id = question['id'])
+
+            question["options"] = []
+
+            for option in options_qs:
+                question["options"].append(model_to_dict(option))
+                del question["options"][-1]["is_correct"]
+                del question["options"][-1]["is_active"]
+
+        return JsonResponse(quiz_details)
