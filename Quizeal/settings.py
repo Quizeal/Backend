d4d3876f66f666287e508fcd20753303995d7f55
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-08-09 15:30:43 +0530

    bug

--- Quizeal/settings.py
+++ Quizeal/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:3000',
