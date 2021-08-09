c0f972caa300b2e31243d0c899aa5d9616b4ce02
siegefried2001 <devasya6032@gmail.com>
2021-08-09 16:16:26 +0530

    Login- not working, no active account found

--- Quizeal/urls.py
+++ Quizeal/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
+    path('', include('jwtapi.urls')),
 ]
