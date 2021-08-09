0ab1329dc316b23df52d4058b107cd641bb3bb1e
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-08-10 02:37:06 +0530

    Merged Main

--- jwtapi/serializer.py
+++ jwtapi/serializer.py
@@ -12,14 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('username','password','first_name', 'last_name','id')    
 
         def create(self, validated_data):
-            user = User.objects.create_user(validated_data['username'],password = validated_data['password'],first_name=validated_data['first_name'],last_name=validated_data['last_name'])
-            # extra_kwargs = {
-            #     'first_name': {'required': True},
-            #     'last_name': {'required': True},
-            #     'username': {'required': True},
-            #     'password': {'required': True}
-            # }
-            #user.password=make_password(user.password)# User serializer
+            User.objects.create_user(validated_data['username'],password = validated_data['password'],first_name=validated_data['first_name'],last_name=validated_data['last_name'])
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
