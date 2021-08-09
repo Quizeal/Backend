0649b92c8380a94dade7ad53d942efa437394cb0
siegefried2001 <devasya6032@gmail.com>
2021-08-10 04:14:18 +0530

    Implemented login endpoints prototype

--- jwtapi/views.py
+++ jwtapi/views.py
@@ -4,13 +4,11 @@ from .serializer import RegisterSerializer, UserSerializer
 # from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.hashers import make_password
 
-
 class RegisterApi(generics.GenericAPIView):
 
     serializer_class = RegisterSerializer
 
     def post(self, request, *args,  **kwargs):
-        request.data._mutable = True
         request.data['password'] = make_password(request.data['password'], None, 'md5')
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
