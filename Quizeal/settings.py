870d766ad52d4f0d39e8c5977bc1a208f482050f
siegefried2001 <devasya6032@gmail.com>
2021-08-10 04:14:55 +0530

    implemented auth endpoints prototype

--- Quizeal/settings.py
+++ Quizeal/settings.py
@@ -121,6 +121,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
 from datetime import timedelta
