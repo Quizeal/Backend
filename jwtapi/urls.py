8ca9920d50a430cf3da5589e13badec96b158746
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-08-18 15:17:17 +0530

    Add Correct Login to Change Password

--- jwtapi/urls.py
+++ jwtapi/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import LoadView, RegisterApi,CustomJWTSerializer
+from .views import LoadUserView, RegisterApi, CustomJWTSerializer
 
 urlpatterns = [
-      path('register/', RegisterApi.as_view()),
-      path('load-user/', LoadView.as_view()),
-      path('login/', jwt_views.TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer), name='token_obtain_pair'),
-      path('token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path("register/", RegisterApi.as_view()),
+    path("load-user/", LoadUserView.as_view()),
+    path(
+        "login/",
+        jwt_views.TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer),
+        name="token_obtain_pair",
+    ),
+    path("token-refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
 ]
