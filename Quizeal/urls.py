d602deac94100a69640c2b67cd4388111e5842c2
daretobedifferent18 <daretobedifferent10920@gmail.com>
2021-08-27 02:00:35 +0530

    Deploy

--- Quizeal/urls.py
+++ Quizeal/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('', include('jwtapi.urls')),
-    path('admin/', admin.site.urls),
+    path("", include("core.urls")),
+    path("", include("jwtapi.urls")),
+    path("admin/", admin.site.urls),
 ]
